10b6e1715967b593cc5758dac98723759d5657cc
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-02-09 20:06:34 +0000

    Add CounterChar

---
+++ src/js/component/CounterChar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CounterChar = ({ children }) => (
+	<div className="counter-char">{children}</div>
+);
+
+export default CounterChar;
--- src/js/component/SecondsCounter.jsx
+++ src/js/component/SecondsCounter.jsx
@@ -1,13 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import padSeconds from "../padSeconds";
+import CounterChar from "./CounterChar.jsx";
 
 const SecondsCounter = () => {
-	const [seconds, setSeconds] = useState(1);
+	const [seconds, setSeconds] = useState(0);
+
+	const clockIcon = <i class="bi bi-clock"></i>;
+	const paddedSeconds = padSeconds(seconds);
+	const counterCharacters = [clockIcon, ...paddedSeconds];
 
 	return (
 		<div>
-			<i class="bi bi-clock"></i>
-			{padSeconds(seconds)}
+			{counterCharacters.map((char) => (
+				<CounterChar> {char} </CounterChar>
+			))}
 		</div>
 	);
 };
