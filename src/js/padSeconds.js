83b15da428caa0744cd19e99cca11c67b1f98937
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-02-09 19:27:24 +0000

    Add documentation

--- src/js/padSeconds.js
+++ src/js/padSeconds.js
@@ -1,5 +1,6 @@
+/** Returns a 6 length string array containing the seconds padded using zeroes */
 function padSeconds(seconds) {
-	return seconds.toString().padStart(6, "0");
+	return seconds.toString().padStart(6, "0").split("");
 }
 
 export default padSeconds;
