7e2a6de9647184a72e15cda2038b99c5509087c5
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-02-09 20:07:03 +0000

    Give functionality to the counter

--- src/js/component/SecondsCounter.jsx
+++ src/js/component/SecondsCounter.jsx
@@ -5,6 +5,14 @@ import CounterChar from "./CounterChar.jsx";
 const SecondsCounter = () => {
 	const [seconds, setSeconds] = useState(0);
 
+	useEffect(() => {
+		setInterval(addSecond, 1000);
+	}, []);
+
+	function addSecond() {
+		setSeconds((prevState) => prevState + 1);
+	}
+
 	const clockIcon = <i class="bi bi-clock"></i>;
 	const paddedSeconds = padSeconds(seconds);
 	const counterCharacters = [clockIcon, ...paddedSeconds];
